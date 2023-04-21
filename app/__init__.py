906b6cb7b67c9708f0f3f7aae7125c24dbae8158
Zhuo <zhuo.parse@gmail.com>
2023-04-21 11:43:43 -0400

    Get all books

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 
+# db = SQLAlchemy()
+# migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     from .routes import hello_world_bp
     app.register_blueprint(hello_world_bp)
+
+    from .routes import books_bp
+    app.register_blueprint(books_bp)
+
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,30 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 hello_world_bp = Blueprint("hello_world", __name__)
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [Book(1, "Daisy", "A good novel."),
+    Book(2, "A Clean Well-lighted Place", "A good short story."),
+    Book(3, "Shanghai Flowers", "A realistic novel about courtesan life.")]
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
 
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
