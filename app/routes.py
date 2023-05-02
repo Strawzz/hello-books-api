6cbaef6e2019836613d5c6702bf4995a1d5d2a12
Zhuo <zhuo.parse@gmail.com>
2023-05-02 12:09:40 -0400

    update one book

--- app/routes.py
+++ app/routes.py
@@ -3,28 +3,74 @@ from app.models.book import Book
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
+# helper functions
 
-@books_bp.route("", methods=["GET","POST"])
+def valid_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    book = Book.query.get(book_id)
+    if not book:
+        abort(make_response({"message":f"book {book_id} not found"}, 404))
+    
+    return book
+
+# route functions
+
+
+@books_bp.route("/<book_id>", methods = ["GET"])
+def handle_book(book_id):
+    book = valid_book(book_id)
+
+    return {
+        "id":book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+@books_bp.route("/<book_id>", methods = ["PUT"])
+def update_book(book_id):
+    book = valid_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")
+
+
+
+
+@books_bp.route("", methods=["GET"])
 def handle_books():
-    if request.method == "GET":
-        books = Book.query.all()
-        books_response = []
-        for book in books:
-            books_response.append({
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            })
-        return jsonify(books_response)
-    elif request.method == "POST":
-        request_body = request.get_json()
-        new_book = Book(title=request_body["title"],
-                        description=request_body["description"])
+    
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
 
-        db.session.add(new_book)
-        db.session.commit()
 
-        return make_response(f"Book {new_book.title} successfully created", 201)
+#     elif request.method == "POST":
+#         request_body = request.get_json()
+#         new_book = Book(title=request_body["title"],
+#                         description=request_body["description"])
+
+#         db.session.add(new_book)
+#         db.session.commit()
+
+#         return make_response(f"Book {new_book.title} successfully created", 201)
+
+
 
 # def handle_books():
 #     books_response = []
@@ -58,14 +104,5 @@ def handle_books():
 #                 "description": book.description
 #             }
 
-# def valid_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         abort(make_response({"message":f"book {book_id} invalid"}, 400))
-    
-#     for book in books:
-#         if book.id == book_id:
-#             return book
-#     abort(make_response({"message":f"book {book_id} not found"}, 404))
+
 
