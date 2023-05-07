e1ed548973e77d70ae29afe1ddf755b3fa497e1b
Zhuo <zhuo.parse@gmail.com>
2023-05-07 15:57:13 -0400

    added query params

--- app/models/book.py
+++ app/models/book.py
@@ -5,3 +5,11 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
+    __tablename__ = "book"
+
+    def to_dict(self):
+        return dict(
+            id = self.id,
+            title = self.title,
+            desctiption = self.description
+        )
--- app/routes.py
+++ app/routes.py
@@ -3,119 +3,111 @@ from app.models.book import Book
 from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
+
 # helper functions
 
 def valid_book(book_id):
+
     try:
         book_id = int(book_id)
     except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+        message = f"book #{book_id} invalid"
+        abort(make_response(f"message: {message}", 400))
 
     book = Book.query.get(book_id)
+
     if not book:
-        abort(make_response({"message":f"book {book_id} not found"}, 404))
+        message = f"book #{book_id} not found"
+        abort(make_response(f"message:{message}", 404))
     
     return book
 
+def valid_paragrams_number(request_body):
+
+    if "title" not in request_body or "description" not in request_body:
+        message = "Please offer all the query paragrams"
+        abort (make_response(f"message: {message}", 400)) 
+    return
+
+
 # route functions
-
-
-@books_bp.route("/<book_id>", methods = ["GET"])
-def handle_book(book_id):
-    book = valid_book(book_id)
-
-    return {
-        "id":book.id,
-        "title": book.title,
-        "description": book.description
-    }
-
-@books_bp.route("/<book_id>", methods = ["PUT"])
-def update_book(book_id):
-    book = valid_book(book_id)
-
-    request_body = request.get_json()
-    
-    # if not request_body["title"] or not request_body["description"]:
-    #     abort(make_response({"message":"invalid input"}, 400))
-    # else:
-    #     book.title = request_body["title"]
-    #     book.description = request_body["description"]
-    
-    book.title = request_body["title"]
-    book.description = request_body["description"]
-    db.session.commit()
-
-    return make_response(f"Book #{book.id} successfully updated")
-
-
-@books_bp.route("/<book_id>", methods = ["DELETE"])
-def delete_book(book_id):
-    book = valid_book(book_id)
-
-    db.session.delete(book)
-    db.session.commit()
-
-    return make_response(f"Book {book.id} successfully deleted")
-
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    
-    books = Book.query.all()
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
-
 
 @books_bp.route("", methods=["POST"])
 def post_a_book():
+
     request_body = request.get_json()
+
+    valid_paragrams_number(request_body)
+    
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
 
     db.session.add(new_book)
     db.session.commit()
 
-    return make_response(jsonify(f"Book {new_book.title} successfully created"), 201)
+    message = f"Book {new_book.title} successfully created"
+    return make_response(jsonify(f"message: {message}"), 201)
+
+
+@books_bp.route("/<book_id>", methods = ["GET"])
+def get_one_book(book_id):
+
+    book = valid_book(book_id)
+
+    return book.to_dict()
+
+
+@books_bp.route("/<book_id>", methods = ["PUT"])
+def update_book(book_id):
+
+    book = valid_book(book_id)
+    request_body = request.get_json()
+    valid_paragrams_number(request_body)
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated", 200)
+
+
+@books_bp.route("/<book_id>", methods = ["DELETE"])
+def delete_book(book_id):
+
+    book = valid_book(book_id)
+
+    db.session.delete(book)
+    db.session.commit()
+
+    message = f"Book #{book.id} successfully deleted"
+
+    return make_response(f"message: {message}", 200)
+
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    
+    request_body = request.get_json()
+    title_query = request_body["title"]
+    description_query = request_body["description"]
+    
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    elif description_query:
+        books = Book.query.filter_by(description=description_query)
+    else:
+        books = Book.query.all()
+        
+    books_response = []
+    for book in books:
+        books_response.append(book.to_dict())
+
+    return jsonify(books_response)
 
 
 
-# def handle_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append({
-#             "id": book.id,
-#             "title": book.title,
-#             "description": book.description
-#         })
-#     return jsonify(books_response)
 
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     # try:
-#     #     book_id = int(book_id)
-#     # except:
-#     #     return {"message":f"book {book_id} invalid"}, 400
 
-#     # for book in books:
-#     #     if book.id == book_id:
-#     #         return {
-#     #             "id": book.id,
-#     #             "title": book.title,
-#     #             "description": book.description
-#     #         }
-#     #     return {"message":f"book {book_id} not found"}, 404
-#     book = valid_book(book_id)
-#     return {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
 
 
 
